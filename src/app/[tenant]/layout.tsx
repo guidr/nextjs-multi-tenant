bdd6f713c18ff2f9535ee613593e7b60e29d641a
Guilherme Dalla Rosa <gui_dr@msn.com>
2025-10-13 22:09:30 +0100

    theme switcher

--- src/app/[tenant]/layout.tsx
+++ src/app/[tenant]/layout.tsx
@@ -1,8 +1,10 @@
+import { ThemeSwitcher } from '@/components/theme-switcher';
+
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <div>
       <div className="fixed top-4 right-4 z-50">
-        Switch
+        <ThemeSwitcher />
       </div>
 
       {children}
